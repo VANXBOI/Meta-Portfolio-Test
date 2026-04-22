b3e45af56a40a2b6f0e5b63dc3bced0cb0bcb1d3
VANXBOI <rock1234.in@gmail.com>
2026-04-22 16:59:06 +0530

    Adding Featured Projects With No Clickability At the Moment

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,37 @@
-<VStack bg="white" borderRadius="lg" overflow="hidden" align="start">
-  <Image src={imageSrc} alt={title} />
+import { HStack, VStack, Image, Heading, Text } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-  <VStack p={4} align="start" spacing={4}>
-    <Heading size="md">{title}</Heading>
-    <Text color="gray.600">{description}</Text>
+const Card = ({ title, description, imageSrc }) => {
+  return (
+    <VStack
+      align="start"
+      bg="white"
+      borderRadius="lg"
+      overflow="hidden"
+      spacing={0}
+    >
+      {/* Image */}
+      <Image src={imageSrc} alt={title} w="100%" h="200px" objectFit="cover" />
 
-    <HStack>
-      <Text fontWeight="bold">See more</Text>
-      <FontAwesomeIcon icon={faArrowRight} size="1x" />
-    </HStack>
-  </VStack>
-</VStack>
+      {/* Content */}
+      <VStack align="start" p={4} spacing={3}>
+        <Heading size="md" color="black">
+          {title}
+        </Heading>
+
+        <Text color="gray.600" fontSize="sm">
+          {description}
+        </Text>
+
+        {/* See more */}
+        <HStack spacing={2} color="black" fontWeight="medium">
+          <Text>See more</Text>
+          <FontAwesomeIcon icon={faArrowRight} size="1x" />
+        </HStack>
+      </VStack>
+    </VStack>
+  );
+};
+
+export default Card;
--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,9 +1,47 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, SimpleGrid } from "@chakra-ui/react";
+import Card from "./Card";
+
+const projects = [
+  {
+    title: "React Space",
+    description:
+      "Handy tool belt to create amazing AR components in a React app, with redux integration via middleware",
+    imageSrc:
+      "https://images.unsplash.com/photo-1581291518857-4e27b48ff24e",
+  },
+  {
+    title: "React Infinite Scroll",
+    description:
+      "A scrollable bottom sheet with virtualisation support, native animations at 60 FPS and fully implemented in JS land",
+    imageSrc:
+      "https://images.unsplash.com/photo-1518770660439-4636190af475",
+  },
+  {
+    title: "Photo Gallery",
+    description:
+      "A One-stop shop for photographers to share and monetize their photos, allowing them to have a second source of income",
+    imageSrc:
+      "https://images.unsplash.com/photo-1492724441997-5dc865305da7",
+  },
+  {
+    title: "Event planner",
+    description:
+      "A mobile application for leisure seekers to discover unique events and activities in their city with a few taps",
+    imageSrc:
+      "https://images.unsplash.com/photo-1503602642458-232111445657",
+  },
+];
 
 export default function ProjectsSection() {
   return (
-    <Box id="projects-section" p={8}>
-      <Heading>Projects</Heading>
+    <Box id="projects-section" bg="purple.900" color="white" p={10}>
+      <Heading mb={6}>Featured Projects</Heading>
+
+      <SimpleGrid columns={[1, 2]} spacing={6}>
+        {projects.map((project) => (
+          <Card key={project.title} {...project} />
+        ))}
+      </SimpleGrid>
     </Box>
   );
 }
